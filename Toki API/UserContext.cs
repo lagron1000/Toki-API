cc584fcd27fdc59ac2141cf95e2c80503c5dafd8
lagron1000 <lagron1000@gmail.com>
2022-06-10 16:35:42 +0300

    Initializing DB

---
+++ Toki API/UserContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Toki_API.Models;
+
+namespace Toki_API
+{
+    public class UserContext : DbContext
+    {
+        private const string connectionString = "server=localhost;post=3306;database=User;user=root;password=123";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(connectionString, MariaDbServerVersion.AutoDetect(connectionString));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasKey(e => e.Id);
+        }
+
+        public DbSet<User>? Users { get; set; }
+    }
+}
